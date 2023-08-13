f1689b26cbfbc3ec5589af5dd483cd148852d6a4
Logan Mohseni <mohsenil85@gmail.com>
2023-08-13 07:29:49 -0600

    use straight instead of quelpa

---
+++ early-init.el
@@ -0,0 +1,2 @@
+(setq package-enable-at-startup nil)
+(setq custom-file  "~/.emacs.d/var/custom" )
--- init.el
+++ init.el
@@ -1,38 +1,50 @@
-(unless (require 'quelpa nil t)
-  (with-temp-buffer
-    (url-insert-file-contents "https://framagit.org/steckerhalter/quelpa/raw/master/bootstrap.el")
-    (eval-buffer)))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-(quelpa
- '(quelpa-use-package
-   :fetcher git
-   :url "https://framagit.org/steckerhalter/quelpa-use-package.git"))
-(require 'quelpa-use-package)
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
 
 
-(setq custom-file  "~/.emacs.d/var/custom" )
-   
+;; (unless (require 'quelpa nil t)
+;;   (with-temp-buffer
+;;     (url-insert-file-contents "https://framagit.org/steckerhalter/quelpa/raw/master/bootstrap.el")
+;;     (eval-buffer)))
 
-(setq package-enable-at-startup nil)
+;; (quelpa
+;;  '(quelpa-use-package
+;;    :fetcher git
+;;    :url "https://framagit.org/steckerhalter/quelpa-use-package.git"))
+;; (require 'quelpa-use-package)
 
-(setq package-archives '(("gnu"       . "https://elpa.gnu.org/packages/")
-			 ("melpa"     . "https://melpa.org/packages/")
-			 ;;("org"       . "http://orgmode.org/elpa/")
-			 ))
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(eval-when-compile
-  (require 'use-package))
+;; (setq package-archives '(("gnu"       . "https://elpa.gnu.org/packages/")
+;; 			 ("melpa"     . "https://melpa.org/packages/")
+;; 			 ;;("org"       . "http://orgmode.org/elpa/")
+;; 			 ))
+
+;; (unless (package-installed-p 'use-package)
+;;   (package-refresh-contents)
+;;   (package-install 'use-package))
+;; (eval-when-compile
+;;   (require 'use-package))
 
 (use-package no-littering :ensure t)
 
 ;;used in debugging
 ;;(use-package evil :ensure t :config (evil-mode 1))
 ;;(setq debug-on-error t)
-;(use-package org :ensure t)	
 
+(use-package org :ensure t)	
 (require 'ob-tangle)
 
 
