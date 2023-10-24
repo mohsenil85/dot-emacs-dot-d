ed47c3109b9bb81d0d023ca4be1dd6e86b179a85
Logan Mohseni <logan.k.mohseni@xcelenergy.com>
2023-10-24 12:15:04 -0600

    call function after defining it

--- init.el
+++ init.el
@@ -900,7 +900,6 @@
   ;;       #'command-completion-default-include-p)
 
 
-  (full-frame-irregardless)
   (blink-cursor-mode -1)
   (defalias 'yes-or-no-p 'y-or-n-p)
   (delete-selection-mode 1)
@@ -1304,6 +1303,7 @@ Position the cursor at it's beginning, according to the current mode."
 (defun full-frame-irregardless ()
   (interactive)
   (set-frame-parameter nil 'fullscreen 'fullboth))
+(full-frame-irregardless)
 
 (defun pulse-line (&rest _)
   "Pulse the current line."
